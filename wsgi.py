9f65298888cd9251a797d9741d034ebfb9643d53
Emin <99510258+emin8800@users.noreply.github.com>
2026-04-09 11:23:22 +0400

    Import settings from backend in wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
-
+from backend.settings import *
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
