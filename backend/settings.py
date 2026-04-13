2c62f5533639b22d79cce2946e74054a94909ac1
Emin <99510258+emin8800@users.noreply.github.com>
2026-04-13 14:24:30 +0400

    Add CSRF trusted origins for backend

--- backend/settings.py
+++ backend/settings.py
@@ -15,7 +15,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://yusif-temir-tikinti-backend.fly.dev",
+]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
