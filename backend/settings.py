5d284a9ef7880181f8e29b07b2da5a90cab07a29
Emin <99510258+emin8800@users.noreply.github.com>
2026-04-15 14:57:28 +0400

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -82,13 +82,22 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'yusiftemirtikinti_db',
+        'USER': 'yusiftemirtikinti_db_user',
+        'PASSWORD': '2N0lbPfeB54wxjHailJherZvijtawZpS',
+        'HOST': 'dpg-d7fmsqosfn5c73d5akl0-a.virginia-postgres.render.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
