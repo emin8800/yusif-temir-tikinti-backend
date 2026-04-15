b66e39c31abb2fd6f59c6ae237d6672b5d00e05d
Emin <99510258+emin8800@users.noreply.github.com>
2026-04-15 11:16:35 +0400

    Update views.py

--- works/views.py
+++ works/views.py
@@ -1,12 +1,17 @@
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Work
 from .serializers import WorkSerializer
 
-@csrf_exempt
+
 @api_view(['GET'])
 def work_list(request):
     works = Work.objects.all()
-    serializer = WorkSerializer(works, many=True)
+
+    serializer = WorkSerializer(
+        works,
+        many=True,
+        context={'request': request}  # 🔥 BURASI VACİBDİR
+    )
+
     return Response(serializer.data)
